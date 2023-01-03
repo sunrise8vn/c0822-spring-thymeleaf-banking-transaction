d12db0078f0be5daf00e7bf04663c622f2314a58
sunrise8vn <sunrise8vn@gmail.com>
2023-01-03 16:53:13 +0700

    Init

---
+++ src/main/java/com/cg/model/Customer.java
@@ -0,0 +1,101 @@
+package com.cg.model;
+
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String phone;
+
+    @Column(precision = 12, scale = 0, nullable = false)
+    private BigDecimal balance;
+
+    private String address;
+
+
+    @OneToMany
+    private List<Deposit> deposits;
+
+    public Customer() {
+    }
+
+    public Customer(Long id, String fullName, String email, String phone, BigDecimal balance, String address, List<Deposit> deposits) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.balance = balance;
+        this.address = address;
+        this.deposits = deposits;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Deposit> getDeposits() {
+        return deposits;
+    }
+
+    public void setDeposits(List<Deposit> deposits) {
+        this.deposits = deposits;
+    }
+}
---
+++ src/main/java/com/cg/model/Deposit.java
@@ -0,0 +1,54 @@
+package com.cg.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+
+@Entity
+@Table(name = "deposits")
+public class Deposit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(targetEntity = Customer.class)
+    @JoinColumn(name = "customer_id", referencedColumnName = "id", nullable = false)
+    private Customer customer;
+
+    @Column(name = "transaction_amount", precision = 12, scale = 0, nullable = false)
+    private BigDecimal transactionAmount;
+
+    public Deposit() {
+    }
+
+    public Deposit(Long id, Customer customer, BigDecimal transactionAmount) {
+        this.id = id;
+        this.customer = customer;
+        this.transactionAmount = transactionAmount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public BigDecimal getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public void setTransactionAmount(BigDecimal transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+}
